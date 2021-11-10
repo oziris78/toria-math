1fbd5d7892004e3bf386d2d889d8520c98b464af
OziRis78 <oguzhantg3@gmail.com>
2021-11-10 13:34:42 +0300

    ⌚ organization for TWebUtil

--- src/main/java/com/telek/telekutils/swing/TDesktopUtils.java
+++ src/main/java/com/telek/telekutils/swing/TDesktopUtils.java
@@ -1,6 +1,27 @@
 package com.telek.telekutils.swing;
 
+import java.awt.*;
+import java.net.URI;
+
+
 public class TDesktopUtils {
+
+
+    public static void openOnBrowser(URI linkURI){
+        try{
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE))
+                Desktop.getDesktop().browse(linkURI);
+        }
+        catch (Exception e){ e.printStackTrace(); }
+    }
+
+
+    public static void openOnBrowser(String link){
+        try{ openOnBrowser(new URI(link)); }
+        catch (Exception e){ e.printStackTrace(); }
+    }
+
+
 
     public static boolean isWindows() {
         return System.getProperty("os.name").startsWith("Windows");
